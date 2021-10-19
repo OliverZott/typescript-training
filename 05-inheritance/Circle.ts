501e38d992652ec7d37942216ef3b340e3ccad8b
Oliver Zott <zott_oliver@web.de>
2021-10-19 13:23:25 +0200

    Inheritance example added.

---
+++ 05-inheritance/Circle.ts
@@ -0,0 +1,19 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape {
+
+    constructor(xValue: number, yValue: number, private _radius: number) {
+        super(xValue, yValue);
+    }
+
+    public get radius(): number {
+        return this._radius
+    }
+    public set radius(value: number) {
+        this._radius = value
+    }
+
+    getInfo(): string {
+        return super.getInfo() + `, radius=${this._radius}`
+    }
+}
